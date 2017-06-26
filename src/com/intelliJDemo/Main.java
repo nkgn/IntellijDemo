1967c79b1fee06e1966bc219b52f72f105913817
nkgn <n_kgn@yahoo.com>
2017-06-25 23:24:12 -0700

    Add hello world message

--- src/com/intelliJDemo/Main.java
+++ src/com/intelliJDemo/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello World");
         System.out.println("hello World2");
+        System.out.println("hello World3");
     }
 }
